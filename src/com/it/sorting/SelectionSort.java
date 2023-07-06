ad083dfe3998de9f08df4bfa66bca1cf7b825b58
hrutik.gurav <hrutik.gurav@viraltech.in>
2023-07-06 11:15:33 +0530

    second commit of the day

--- src/com/it/sorting/SelectionSort.java
+++ src/com/it/sorting/SelectionSort.java
@@ -20,8 +20,28 @@ public class SelectionSort {
 		
 	}
 
+	public static void selectionDescending(int arr[]) {
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			int smallest = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[smallest] < arr[j]) {
+					smallest = j;
+				}
+			}
+
+			int temp = arr[smallest];
+			arr[smallest] = arr[i];
+			arr[i] = temp;
+		}
+		GenericeMethods.printArray(arr);
+		
+	}
+
+	
 	public static void main(String[] args) {
 		int arr[] = { 1,9,8,2,48,69,2,1};
-		selectionAscending(arr);
+		//selectionAscending(arr);
+		selectionDescending(arr);
 	}
 }
